9a2d40d9f992bd28038679d2533152f01933c033
Vicnent GOUGEON <gougeonvincent31@gmail.com>
2022-01-26 15:16:36 +0100

    LOG

--- apps/api/src/app/auth.controller.ts
+++ apps/api/src/app/auth.controller.ts
@@ -4,6 +4,8 @@ const router = Router()
 
 router.post('/login', (req, res) => {
     const code: string = req.body.code;
+    console.log('CLIENT ID : ', process.env.NX_GITHUB_CLIENT_ID)
+    console.log('CLIENT SECRET : ', process.env.NX_GITHUB_SECRET)
     axios.post('https://github.com/login/oauth/access_token', {
         client_id: process.env.NX_GITHUB_CLIENT_ID,
         client_secret: process.env.NX_GITHUB_SECRET,
