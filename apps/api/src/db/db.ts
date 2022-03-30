fefb4f34b6fd8186198a7aebb1cc04c54249dd6f
Vicnent GOUGEON <gougeonvincent31@gmail.com>
2022-03-30 13:12:29 +0200

    db

--- apps/api/src/db/db.ts
+++ apps/api/src/db/db.ts
@@ -1,6 +1,12 @@
 import { Model } from "objection";
 import knex from 'knex'
 
+console.log("CONNECTING TO DB")
+console.log("HOST", process.env.NX_DB_HOST)
+console.log("USER", process.env.NX_DB_USER)
+console.log("PASSWORD", process.env.NX_DB_PASSWORD)
+console.log("NAME", process.env.NX_DB_NAME)
+console.log("PORT", process.env.NX_DB_PORT)
 const db = knex({
   client: 'mysql2',
   useNullAsDefault: true,
